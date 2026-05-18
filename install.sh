a21971c9261b9383db4c78285e6b234ab4f8346d
Ammar Hussein <mar@mavis.eternity>
2026-05-17 20:42:06 -0400

    Added hint for selected windows

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-for d in i3 kitty nvim polybar rofi; do
+for d in i3 kitty nvim picom polybar rofi; do
     ln -sfn "$DOTFILES/$d" "$HOME/.config/$d"
 done
